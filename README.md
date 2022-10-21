bd08a943e0d168062b8d6cd7357b18d911ac4fcb
Muhammad Abdullah <39946504+abdullahwale@users.noreply.github.com>
2022-10-21 10:06:44 +0500

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# my_course_quizapp
-Its YouTube Tutorial Playlist 
--- simple_quizap_2022_seprate/android/app/src/main/kotlin/com/example/simple_quizap_2022_seprate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.simple_quizap_2022_seprate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
